550f16967a149bea7c267e51c40432547ac9f3b6
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-06-29 23:21:02 +1000

    Another extension description fix

--- install.py
+++ install.py
@@ -15,7 +15,7 @@ import configobj
 
 from weecfg.extension import ExtensionInstaller
 
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 
 CONFIG = """
 [AggregatedValues]
@@ -58,7 +58,7 @@ class AggregatedValuesInstaller(ExtensionInstaller):
             'version': VERSION,
             'name': 'AggregatedValues',
             # add a leading space, so that long versions does not run into the description
-            'description': ' Calculate aggregated values that other plugins can then use without duplicating functionality and database lookups',
+            'description': ' Calculate aggregated values that other extensions can then use without duplicating functionality and database lookups',
             'author': "John Smith",
             'author_email': "deltafoxtro256+AggregatedValues@gmail.com",
             'files': [('bin/user', ['bin/user/AggregatedValues.py'])]
